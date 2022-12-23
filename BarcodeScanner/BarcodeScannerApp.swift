4d2882581437bc73f2adf5b461979e0e8dcb6703
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-23 16:48:28 +0000

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Rodrigues, Luis (EXTERN: EEDS) on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
