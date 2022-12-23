3f874d7a73cbff8a6075096ce83889eccdaaf18d
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-23 21:04:44 +0000

    Added main view for barcode scanning and customised camera permission string

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,34 @@
+//
+//  BarcodeScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Rodrigues, Luis on 23/12/2022.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+			VStack {
+				Rectangle()
+					.frame(maxWidth: .infinity, maxHeight: 300)
+				Spacer().frame(height: 60)
+				Label("Scanned barcode:", systemImage: "barcode.viewfinder")
+					.font(.title)
+				Text("Not yet scanned")
+					.bold()
+					.font(.largeTitle)
+					.foregroundColor(.green)
+					.padding()
+			}
+			.navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
