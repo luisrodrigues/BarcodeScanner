ad203c3b42108f11c1c8562c24e6e4e9a2b72ec3
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-24 11:36:46 +0000

    Added ScannerView and config to test using real device

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,37 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Rodrigues, Luis on 24/12/2022.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+	
+	func makeUIViewController(context: Context) -> ScannerVC {
+		ScannerVC(scannerDelegate: context.coordinator)
+	}
+	
+	func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+	
+	func makeCoordinator() -> Coordinator {
+		Coordinator()
+	}
+	
+	final class Coordinator: NSObject, ScannerVCDelegate {
+		func didFind(barcode: String) {
+			print(barcode)
+		}
+		
+		func didSurface(error: CameraError) {
+			print(error.rawValue)
+		}
+	}
+}
+
+struct ScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerView()
+    }
+}
