398fd9d8decb4661675a180af7f93f09fdefe0e4
Luís Rodrigues <luis.cnm@gmail.com>
2022-12-24 23:59:42 +0000

    Finished Coordinator code

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
+	
+	@State private var scannedCode = ""
+	
     var body: some View {
         NavigationView {
 			VStack {
-				ScannerView()
+				ScannerView(scannedCode: $scannedCode)
 					.frame(maxWidth: .infinity, maxHeight: 300)
+				
 				Spacer().frame(height: 60)
+				
 				Label("Scanned barcode:", systemImage: "barcode.viewfinder")
 					.font(.title)
-				Text("Not yet scanned")
+				
+				Text(scannedCode.isEmpty ? "Not yet scanned" : scannedCode)
 					.bold()
 					.font(.largeTitle)
-					.foregroundColor(.green)
+					.foregroundColor(scannedCode.isEmpty ? .red : .green)
 					.padding()
 			}
 			.navigationTitle("Barcode Scanner")
